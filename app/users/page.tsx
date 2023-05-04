caa1ac7d5901b5b4b676d136996e5ba0edf6cd2a
Aron Teh <aronteh.chess@gmail.com>
2023-05-04 10:58:48 +0100

    Add metadata to users page. Minor

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,6 +2,12 @@ import { prisma } from "@/lib/prisma";
 import styles from "./page.module.css";
 import React from "react";
 import UserCard from "@/components/UserCard/UserCard";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "NextSpace - Users",
+  description: "Users page for NextSpace",
+};
 
 export default async function UserPage() {
   // Retrieve the users JSON data directly from a server component
